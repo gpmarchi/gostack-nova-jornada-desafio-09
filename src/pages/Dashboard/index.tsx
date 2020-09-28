4d83a6e0afda3ec65ec046e8647d9aadb9899cba
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-28 15:50:13 -0300

    Delete a food plate

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -65,7 +65,9 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    setFoods(foods.filter(currentFood => currentFood.id !== id));
   }
 
   function toggleModal(): void {
