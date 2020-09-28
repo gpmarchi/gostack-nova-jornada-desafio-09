937cd04ca0e0cef4ce772827eb38d63d93b08b28
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-28 16:26:51 -0300

    Toggle food plate availability

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -4,6 +4,8 @@ import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 
+import api from '../../services/api';
+
 interface IFoodPlate {
   id: number;
   name: string;
@@ -29,7 +31,9 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    setIsAvailable(!isAvailable);
+
+    // await api.patch(`/foods/${food.id}`, { available: !isAvailable });
   }
 
   function setEditingFood(): void {
